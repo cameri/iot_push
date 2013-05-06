46d0d50d3169d75c1c73094456bebf277cf72bff
Ricardo Arturo Cabral <ricardo.arturo.cabral@gmail.com>
2013-05-06 00:01:08 -0300

    Added missing license

--- rpi2cosm_to_rpi2pachube.sh
+++ rpi2cosm_to_rpi2pachube.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#    This file is part of rpi2pachube.
+#    rpi2pachube - Script for pushing Raspberry Pi data to Pachube
+#    Copyright (c) 2012, Ricardo Cabral <ricardo.arturo.cabral@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 which realpath &> /dev/null
 if [ $? -eq 0 ]; then
   realpath=`realpath $0 2>/dev/null`
